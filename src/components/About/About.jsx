269bceb43fe20abbbb115b1c33f69678b6af96e9
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-06-28 03:56:17 +0530

    about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,26 @@
+import "./About.css";
+import yash from "../../assets/yash.png";
+import manas from "../../assets/manas.jpeg";
+
 function About() {
-    return (
-        <div>About</div>
-    )
+  return (
+    <section id="about">
+      <div className="card-container">
+        <div className="card">
+          <img src={yash} className="card-img" alt="Yash" />
+          <h1 className="name">Yash</h1>
+        </div>
+        <div className="card">
+          <img src={manas} className="card-img" alt="Manas" />
+          <h1 className="name">Manas</h1>
+        </div>
+      </div>
+      <div className="card company-card">
+        <h1 className="company-name">Company XYZ</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque auctor, erat non sagittis interdum, urna erat vehicula justo, eget cursus lacus leo at nisi. Integer et nulla et felis gravida scelerisque.</p>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
